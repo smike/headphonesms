03919406264c96b281a88539eabc5a264a2e5cba
smike <smike@alum.mit.edu>
2011-11-12 22:52:08 -0500

    Adding caller ID announcements.

--- src/com/smike/headphonesms/HeadphoneSmsApp.java
+++ src/com/smike/headphonesms/HeadphoneSmsApp.java
@@ -14,41 +14,65 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract.PhoneLookup;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class HeadphoneSmsApp extends BroadcastReceiver {
   public static final String MESSAGES_EXTRA = "com.smike.headphonesms.MESSAGES";
 
   private static final String LOG_TAG = "HeadphoneSmsApp";
-  private static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
+  private static final String SMS_ACTION = "android.provider.Telephony.SMS_RECEIVED";
+  private static final String CALL_ACTION = "android.intent.action.PHONE_STATE";
 
   public void onReceive(Context context, Intent intent) {
-    if (intent.getAction().equals(ACTION)) {
-      if (shouldRead(context)) {
-        Log.i(LOG_TAG, "Headset connected, reading SMS");
-        Bundle bundle = intent.getExtras();
-        if (bundle != null) {
-          Object pdus[] = (Object[]) bundle.get("pdus");
-          ArrayList<String> messages = new ArrayList<String>();
-          for (Object pdu : pdus) {
-            SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdu);
-            String from = getContactNameFromNumber(smsMessage.getDisplayOriginatingAddress(),
-                                                   context.getContentResolver());
+    if (shouldRead(context)) {
+      Bundle bundle = intent.getExtras();
+      if (bundle == null) {
+        Log.w(LOG_TAG, "Bundle is null. Doing nothing.");
+        return;
+      }
 
-            String text = "Received SMS from " + from + ". " + smsMessage.getDisplayMessageBody();
+      ArrayList<String> messages = new ArrayList<String>();
+      if (intent.getAction().equals(SMS_ACTION)) {
+        TelephonyManager telephonyManager =
+            (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (telephonyManager.getCallState() == TelephonyManager.CALL_STATE_OFFHOOK) {
+          Log.i(LOG_TAG, "Not reading SMS because the there is an ongoing call.");
+          return;
+        }
 
-            messages.add(text);
-          }
+        Log.i(LOG_TAG, "SMS received, reading SMS.");
+        Object pdus[] = (Object[]) bundle.get("pdus");
+        for (Object pdu : pdus) {
+          SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdu);
+          String from = getContactNameFromNumber(smsMessage.getDisplayOriginatingAddress(),
+                                                 context.getContentResolver());
 
-          Intent readSmsIntent = new Intent(context, ReadSmsService.class);
-          readSmsIntent.putStringArrayListExtra(MESSAGES_EXTRA, messages);
-          context.startService(readSmsIntent);
+          String text = "Received SMS from " + from + ": " + smsMessage.getDisplayMessageBody();
+          messages.add(text);
+        }
+      } else if (intent.getAction().equals(CALL_ACTION)) {
+        Log.i(LOG_TAG, "Call received connected, announcing caller.");
+
+        String state = bundle.getString(TelephonyManager.EXTRA_STATE);
+        if (state.equalsIgnoreCase(TelephonyManager.EXTRA_STATE_RINGING)) {
+          String phonenumber = bundle.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+          String from = getContactNameFromNumber(phonenumber, context.getContentResolver());
+
+          String text = "Receiving call from " + from + ".";
+          messages.add(text);
         }
       } else {
-        Log.i(LOG_TAG, "Headset not connected, doing nothing");
+        Log.w(LOG_TAG, "Received unrecognized action broadcast: " + intent.getAction());
+      }
+
+      if (!messages.isEmpty()) {
+        Intent readSmsIntent = new Intent(context, ReadSmsService.class);
+        readSmsIntent.putStringArrayListExtra(MESSAGES_EXTRA, messages);
+        context.startService(readSmsIntent);
       }
     } else {
-      Log.w(LOG_TAG, "Received unrecognized action broadcast: " + intent.getAction());
+      Log.i(LOG_TAG, "Headset not connected, doing nothing");
     }
   }
 
