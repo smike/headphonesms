2487ee082d2dce51e236e1c53c12e7954832e99c
smike <smike@alum.mit.edu>
2012-06-12 00:53:14 -0400

    Adding Bluetooth timeout for devices where SCO seems present but does not respond for a long time (I think this is a bug in vendors' code not conforming to the API).

--- src/com/smike/headphonesms/ReadSmsService.java
+++ src/com/smike/headphonesms/ReadSmsService.java
@@ -3,6 +3,8 @@ package com.smike.headphonesms;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Service;
 import android.content.Context;
@@ -15,6 +17,7 @@ import android.speech.tts.TextToSpeech;
 import android.util.Log;
 
 public class ReadSmsService extends Service {
+  private static final String BLUETOOTH_TIMEOUT_EXTRA = "com.smike.headphonesms.BLUETOOTH_TIMEOUT";
   private static final String QUEUE_MESSAGE_EXTRA = "com.smike.headphonesms.QUEUE_MESSAGE";
   private static final String START_READING_EXTRA = "com.smike.headphonesms.START_READING";
   private static final String STOP_READING_EXTRA = "com.smike.headphonesms.STOP_READING";
@@ -24,6 +27,7 @@ public class ReadSmsService extends Service {
 
   private final LocalBinder binder = new LocalBinder();
   private Queue<String> messageQueue = new LinkedList<String>();
+  private TimerTask bluetoothTimerTask;
 
   private TextToSpeech tts;
 
@@ -49,15 +53,23 @@ public class ReadSmsService extends Service {
   public int onStartCommand(Intent intent, int flags, int startId) {
     if (intent == null) {
       // Service restarted after suspension. Nothing to do.
-      return START_STICKY;
+      stopSelf();
+      return START_NOT_STICKY;
     }
 
     synchronized(messageQueue) {
-      if (intent.hasExtra(STOP_READING_EXTRA)) {
-        messageQueue.clear();
+      if (intent.hasExtra(BLUETOOTH_TIMEOUT_EXTRA)) {
+        if (bluetoothTimerTask != null) {
+          audioManager.stopBluetoothSco();
+          startReading(this);
+          bluetoothTimerTask.cancel();
+          bluetoothTimerTask = null;
+        }
+      } else if (intent.hasExtra(STOP_READING_EXTRA)) {
         if (tts != null) {
           // This will trigger onUtteranceCompleted, so we don't have to worry about cleaning up.
           tts.stop();
+          messageQueue.clear();
         }
 
         // We still want to stick around long enough for onUtteranceCompleted to get called, so let
@@ -75,14 +87,29 @@ public class ReadSmsService extends Service {
             ReadSmsService.startReading(this);
           } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO &&
               audioManager.isBluetoothScoAvailableOffCall()) {
-            Log.i(LOG_TAG, "Starting SCO, will wait until it is connected.");
+            Log.i(LOG_TAG, "Starting SCO, will wait until it is connected. sco on: " +
+                audioManager.isBluetoothScoOn());
             audioManager.startBluetoothSco();
+            bluetoothTimerTask = new TimerTask() {
+              @Override
+              public void run() {
+                ReadSmsService.bluetoothTimeout(ReadSmsService.this);
+              }
+            };
+            Timer timer = new Timer("bluetoothTimeoutTimer");
+            timer.schedule(bluetoothTimerTask, 5000);
           } else if (HeadphoneSmsApp.shouldRead(false, this)) {
             // In case we should read anyway (reading is always on)
             ReadSmsService.startReading(this);
           }
         }
       } else if (intent.hasExtra(START_READING_EXTRA)) {
+        if (bluetoothTimerTask != null) {
+          // Probably triggered by a bluetooth connection. reset;
+          bluetoothTimerTask.cancel();
+          bluetoothTimerTask = null;
+        }
+
         if (tts == null && !messageQueue.isEmpty()) {
           tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
@@ -129,7 +156,7 @@ public class ReadSmsService extends Service {
 
   private void speak(final String text) {
     // The first message should clear the queue so we can start speaking right away.
-    Log.i(LOG_TAG, "speaking " + text);
+    Log.i(LOG_TAG, "speaking \"" + text + "\"");
     final HashMap<String, String> params = new HashMap<String, String>();
     params.put(TextToSpeech.Engine.KEY_PARAM_STREAM,
                String.valueOf(AudioManager.STREAM_VOICE_CALL));
@@ -141,22 +168,27 @@ public class ReadSmsService extends Service {
 
   public static void queueMessage(String message, Context context) {
     Log.i(LOG_TAG, "Queueing message: " + message);
-    Intent readSmsIntent = new Intent(context, ReadSmsService.class);
-    readSmsIntent.putExtra(ReadSmsService.QUEUE_MESSAGE_EXTRA, message);
-    context.startService(readSmsIntent);
+    sendIntent(ReadSmsService.QUEUE_MESSAGE_EXTRA, message, context);
   }
 
   public static void startReading(Context context) {
     Log.i(LOG_TAG, "Starting to read message");
-    Intent readSmsIntent = new Intent(context, ReadSmsService.class);
-    readSmsIntent.putExtra(ReadSmsService.START_READING_EXTRA, "");
-    context.startService(readSmsIntent);
+    sendIntent(ReadSmsService.START_READING_EXTRA, null, context);
   }
 
   public static void stopReading(Context context) {
     Log.i(LOG_TAG, "Stopping reading of messages");
+    sendIntent(ReadSmsService.STOP_READING_EXTRA, null, context);
+  }
+
+  public static void bluetoothTimeout(Context context) {
+    Log.i(LOG_TAG, "Timedout waiting for bluetooth.");
+    sendIntent(ReadSmsService.BLUETOOTH_TIMEOUT_EXTRA, null, context);
+  }
+
+  private static void sendIntent(String extraName, String extraValue, Context context) {
     Intent readSmsIntent = new Intent(context, ReadSmsService.class);
-    readSmsIntent.putExtra(ReadSmsService.STOP_READING_EXTRA, "");
+    readSmsIntent.putExtra(extraName, extraValue);
     context.startService(readSmsIntent);
   }
 }
